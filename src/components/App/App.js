c09a7555ff0c403a3c7f5d96f559da20596c5d22
techsavvysos <kedogosospeter36@gmail.com>
2023-05-15 17:47:13 +0100

    added few more tweaks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,51 +1,76 @@
 import './App.css';
-import { FaPlay, FaStop, FaGithub,FaInstagram, FaLinkedin, FaTwitter, FaQuoteLeft, FaQuoteRight, FaEnvelope } from "react-icons/fa";
+import { FaPlay, FaStop, FaGithub,FaInstagram, FaLinkedin, FaTwitter, FaQuoteLeft, FaQuoteRight, FaEnvelope, FaBars,FaTimes } from "react-icons/fa";
 
 
 function App() {
 
   const videoID = "n4b8FRUDNZo"; 
 
+
+  const handleCloseClick = () => {
+    document.getElementById("nav").style.height = "0%";
+    if(window.innerWidth < 738){
+      document.getElementById("open").style.display = "block";
+    }
+
+    
+  }
+
+  const handleOpenClick = () => {
+    document.getElementById("nav").style.height = "100%";
+    document.getElementById("open").style.display = "none";
+    
+  }
+
+  const audio = document.getElementById("myaudio1");
+  const playbutton = document.getElementById("playbutton1");
+
+  playbutton.addEventListener("click", function(){
+    audio.play();
+  })
+
   return (
     <div className="App">
       <header>
-        <nav className="navbar">
+      <button className='open' id='open' onClick={handleOpenClick}><FaBars/></button>
+        <nav className="navbar" id="nav">
+        <button className='close' id='close' onClick={handleCloseClick}><FaTimes /></button>
           <ul>
-            <li><a href="about.html">About</a></li>
-            <li><a href ="work.html">Work</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href ="#projects">Work</a></li>
+            <li><a href="#contact">Contact</a></li>
           </ul>
         </nav>
-        <div className='main flex-container'>
+        <main className='main'>
           <div className='hero-image'>
             <img src="https://static.vecteezy.com/system/resources/previews/008/878/184/non_2x/icosahedron-geometric-shape-3d-illustration-free-png.png" alt="Icosahedron geometric shape 3D illustration" ></img>
           </div>
           <div className="main-title">
             <p>Hi,</p>
             <h1>I'm Sospeter Kedogo</h1>
-            <p>As a web developer, I love crafting beautiful and engaging websites using HTML,CSS , JavaScript, and React. Let's collaborate to turn your online ideas into reality!</p>
-            <button className='hero-btn'>Ready to chat?</button>
+            <p>I love crafting beautiful websites using HTML, CSS , JavaScript, and React.</p>
+            <button className='hero-btn'>Let's Chat</button>
           </div>
-          </div>        
+          </main>        
       </header>
 
-      <section className='aboutme'>
+      <section id='about'  className='aboutme'>
           <h2>About Me</h2>
-          <p>As a web developer and Cybersecurity student at the University of Northampton, I'm fueled by my passion for coding and creating fascinating projects that showcase my skills. With years of experience in computer science, I have a broad knowledge base, especially in web development and design, as well as SEO and blog writing.</p><p>My love for technology dates back to my childhood, where I spent countless hours tinkering with a Windows XP computer and exploring the possibilities of what it could do. Since then, I've been pursuing my career in tech with a strong sense of curiosity and a drive to push myself to new heights.</p><p>Today, I continue to explore innovative ways to enhance user experiences and stay up-to-date with cutting-edge technologies. I'm excited about the future of web development and cybersecurity, and I'm dedicated to making a significant impact with my expertise and passion.</p>
+          <p>As a web developer and Cybersecurity student at the University of Northampton, I'm fueled by my passion for coding and creating fascinating projects that showcase my skills. I have a broad knowledge base, especially in web design, as well as SEO and blog writing.</p><p>My love for technology dates back to my childhood, where I spent countless hours tinkering with a Windows XP computer and exploring the possibilities of what it could do. Since then, I've been pursuing my career in tech with a strong sense of curiosity and a drive to push myself to new heights.</p><p>Today, I continue to explore innovative ways to enhance user experiences and stay up-to-date with cutting-edge technologies. I'm excited about the future of web development and cybersecurity, and I'm dedicated to making a significant impact with my expertise and passion.</p>
       </section>
 
-      <section>
+      <section >
         <div className='projects'>
-          <p>Behold, the fruits of my labor! Feast your eyes on some of the incredible projects I've completed so far. Take a look and see for yourself!</p>
+          <p>Behold, the fruits of my labor! Feast your eyes on some of the incredible projects I've completed so far.</p>
         </div>
 
-        <div className='calculator'>
-          <div className='calcImage'>
-            <img src="https://cdn-icons-png.flaticon.com/512/4645/4645203.png" alt="calculator display"></img>
-          </div>
+        <div className='calculator' id='projects'>
           <div className='calcText'>
             <h2>Javascript Calculator</h2>
             <p>My calculator is a simple yet powerful tool for performing basic math functions such as addition, subtraction, multiplication, and division. It has a user-friendly interface and a large display that shows the current calculation and final result in real-time.</p>
+          </div>
+          <div className='calcImage'>
+            <img src="https://cdn-icons-png.flaticon.com/512/4645/4645203.png" alt="calculator display"></img>
           </div>
         </div>
 
@@ -76,8 +101,7 @@ function App() {
             <img src="https://upload.wikimedia.org/wikipedia/en/6/60/Firework_cover.png" alt="spinning disc with album icon"></img>
             <p className='title'>Fireworks</p>
             <p className='artist'>Katy Perry</p>
-
-            <button>< FaPlay /></button>
+            <button id='playbutton1'>< FaPlay /></button>
             <button ><FaStop/></button>
           </div>
 
@@ -117,15 +141,15 @@ function App() {
           allowFullScreen
         ></iframe>
         <div className='content'>
-          <h2 >Thriving in the world of competitive mobile gaming is my guilty pleasure.</h2>
+          <h2>Thriving in the world of competitive mobile gaming is my guilty pleasure.</h2>
         </div>
           
       </section>
 
       <footer>
         <h2>Ready to Bring Your Vision to Life?</h2>
-        <p>Looking for a skilled freelancer to help bring your project to the next level? <br/>Let's collaborate and create something amazing together. Reach out today and <a href="email" className='chatlink'>let's chat!</a></p>
-        <ul>
+        <p>Let's collaborate and create something amazing together. Reach out today and <a href="email" className='chatlink'>let's chat!</a></p>
+        <ul id='contact'>
           <li><a href="instagram.html"><FaInstagram/></a></li>
           <li><a href="twitter.html"><FaTwitter/></a></li>
           <li><a href="email.html"><FaEnvelope/></a></li>
